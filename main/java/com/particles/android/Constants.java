9f331dc73481da45d750a208543f8b364ed3a08f
xnvst <ynttmp@gmail.com>
2015-07-22 22:35:53 -0400

    1st commit
    android firework

---
+++ main/java/com/particles/android/Constants.java
@@ -0,0 +1,8 @@
+package com.particles.android;
+
+/**
+ * Created by Feng on 27/06/2015.
+ */
+public class Constants {
+    public static final int BYTES_PER_FLOAT = 4;
+}
---
+++ main/java/com/particles/android/util/LoggerConfig.java
@@ -0,0 +1,8 @@
+package com.particles.android.util;
+
+/**
+ * Created by Feng on 22/06/2015.
+ */
+public class LoggerConfig {
+    public static final boolean ON = true;
+}
